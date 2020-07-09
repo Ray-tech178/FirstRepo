661900fda2a2461f9a4dac956bb60443f22a4bc4
Ray-tech178 <67976062+Ray-tech178@users.noreply.github.com>
2020-07-09 15:38:03 -0500

    first ver
    first ver

---
+++ Pet problem.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//number of dogs
+let dogs = 6 + 2
+//number of cats
+let cats = 5 - 1
+//number of turtles
+let turtles = 2 + 1
+//number of hamsters
+let hamsters = 1 + 1
+//number of pigs
+let pigs = 1
+//number of fishes
+let fishes = 1
+//number of snakes
+let snakes = 1
+
+//total number of animals
+let totalNumberOfAnimals = dogs + cats + turtles + hamsters + pigs + fishes + snakes
+//total number of mammals
+let totalNumberOfMammals = dogs + cats + hamsters +  pigs
