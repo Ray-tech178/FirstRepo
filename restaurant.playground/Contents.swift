06da5e444b1e72d0056aeee24d9e5ea770403fd4
Ray-tech178 <67976062+Ray-tech178@users.noreply.github.com>
2020-07-10 13:38:20 -0500

    First ver
    FIrst ver

---
+++ restaurant.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let customerOrderOne = "fish"
+let customerOrderTwo = "risotto"
+let customerOrderThree = "soup"
+
+let serverResponseToTableOne = "Let me make sure I've got this right: fish, risotto, and soup. Is that everything?"
+
+let tableOneResponse = "Yes, thank you!"
+
+let customerOrderFour = "돌솥비빔밥"
+let customerOrderFive = "Pasztecik szczeciński"
+let customerOrderSix = "小笼包"
+
+let serverResponseToTableTwo = "Let me make sure I've got this right:\(customerOrderFour), \(customerOrderFive), \(customerOrderSix)."
+
+let tableTwoResponse = "Perfect, merci bien."
+
+//test
+print(serverResponseToTableTwo)
