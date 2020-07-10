a3e5de1c965f04139bcf499cccd9ca18baaa6541
Ray-tech178 <67976062+Ray-tech178@users.noreply.github.com>
2020-07-10 13:42:17 -0500

    First ver
    First ver

---
+++ Go! Fight! Win!.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let schoolName = "James Mei"
+let refrain = " Go \(schoolName), beat them all, show them the strength of \(schoolName)"
+
+let song = "\(refrain)\n\(refrain)\nYes, \(refrain)"
+print(song)
